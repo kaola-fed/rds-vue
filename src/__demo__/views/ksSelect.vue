d7b3577055fea2228cf8d348978fb007e4ae34bf
轩芝 <wc234780@alibaba-inc.com>
2020-02-13 12:04:27 +0800

    fix: 下拉组件设置limit回显问题修复

---
+++ src/__demo__/views/ksSelect.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h2 style="text-align: center;">4秒后返回下拉数据</h2>
+
+    <p>[limit:5, v-model={{id0s}}] (v-model值为第[1, 2, 3, 4, 5, 6, 7, 8, 9]项，超出limit范围)</p>
+    <ks-select :source="list" v-model="id0s" :limit="5" multiple />
+    <p>[limit:5, v-model={{id05}}] (5秒后，v-model值为第7项，超出limit范围)</p>
+    <ks-select :source="list" v-model="id05" :limit="5" />
+    <p>[limit:5, v-model={{id03}}] (3秒后，v-model值为第7项，超出limit范围)</p>
+    <ks-select :source="list" v-model="id03" :limit="5" />
+    <p>[limit:5, v-model={{id0}}]（v-model初始值为第7项，超出limit范围）</p>
+    <ks-select :source="list" v-model="id0" :limit="5" />
+    <p>[limit:5, v-model={{id15}}]（5秒后，v-model值为第3项，没超出limit范围）</p>
+    <ks-select :source="list" v-model="id15" :limit="5" />
+    <p>[limit:5, v-model={{id13}}]（3秒后，v-model值为第3项，没超出limit范围）</p>
+    <ks-select :source="list" v-model="id13" :limit="5" />
+    <p>[limit:5, v-model={{id1}}]（v-model初始值为第3项，没超出limit范围）</p>
+    <ks-select :source="list" v-model="id1" :limit="5" />
+    <p>[limit:  , v-model={{id25}}] (5秒后，v-model值为第5项)</p>
+    <ks-select :source="list" v-model="id25" />
+    <p>[limit:  , v-model={{id23}}] (3秒后，v-model值为第5项)</p>
+    <ks-select :source="list" v-model="id23" />
+    <p>[limit:  , v-model={{id2}}] (v-model初始值为第5项)</p>
+    <ks-select :source="list" v-model="id2" />
+  </div>
+</template>
+
+<script>
+import RdsVue from '../../index';
+
+export default {
+  data() {
+    return {
+      list: [],
+      id0ns: [],
+      id0s: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      id0: 7,
+      id03: null,
+      id05: null,
+      id1: 3,
+      id13: null,
+      id15: null,
+      id2: 5,
+      id23: null,
+      id25: null,
+    };
+  },
+  created() {
+    setTimeout(() => { this.id03 = 7; this.id13 = 3; this.id23 = 5; }, 3000);
+    setTimeout(() => { this.id05 = 7; this.id15 = 3; this.id25 = 5; }, 5000);
+    setTimeout(() => {
+      this.list = Array.from(Array(1000)).map((_, i) => ({
+        id: i + 1,
+        name: `这是第${i + 1}项`,
+      }));
+    }, 4000);
+  },
+};
+</script>
+<style lang="scss">
+@include b(header-detail) {
+  @include e(style) {
+    margin-bottom: 0px;
+    .el-form-item {
+      margin-bottom: 0px;
+    }
+  }
+}
+</style>
--- src/components/base/KSSelect/index.ts
+++ src/components/base/KSSelect/index.ts
@@ -1,58 +1,73 @@
-import {
-  Component,
-  Prop,
-  Mixins,
-} from 'vue-property-decorator';
+import { Component, Prop, Mixins } from 'vue-property-decorator';
 
-import { SourceItem } from '../KSSourceProvider';
 import SourceProviderMixin from '../../../mixins/sourceProvider';
 
 @Component
 export default class KsSelect extends Mixins(SourceProviderMixin) {
   public static componentName = 'KsSelect';
 
-  @Prop({ type: String, default: '请选择' }) placeholder!: String;
+  @Prop({ type: String, default: '请选择' }) placeholder!: string;
 
   @Prop({ default: '' }) value;
 
-  @Prop({ type: Boolean, default: false }) multiple!: Boolean;
+  @Prop({ type: Boolean, default: false }) multiple!: boolean;
 
-  @Prop({ type: Boolean, default: true }) filterable!: Boolean;
+  @Prop({ type: Boolean, default: true }) filterable!: boolean;
 
   @Prop({ type: Number, default: 0 }) limit!: number;
 
-  @Prop({ type: String, default: 'small' }) size!: String;
+  @Prop({ type: String, default: 'small' }) size!: string;
 
-  @Prop({ type: String, default: 'id' }) valueKey!: String;
+  @Prop({ type: String, default: 'id' }) valueKey!: string;
 
   @Prop({ type: String, default: 'name' }) labelKey!: string;
 
-  @Prop({ type: Boolean, default: true }) clearable!: Boolean;
+  @Prop({ type: Boolean, default: true }) clearable!: boolean;
 
-  @Prop({ type: Boolean, default: false }) disabled!: Boolean;
+  @Prop({ type: Boolean, default: false }) disabled!: boolean;
 
   protected filterInput: any = '';
 
   protected get filteredSource() {
-    if (!this.filterInput) {
+    if (!this.filterInput && (!this.value || (this.multiple && !this.value.length))) {
       return this.computedSource;
     }
-    const input = String(this.filterInput).replace(/[|\\{}()[\]^$+*?.]/g, '\\$&');
+
+    const input = String(this.filterInput).replace(
+      /[|\\{}()[\]^$+*?.]/g,
+      '\\$&',
+    );
     const regexp = new RegExp(input, 'i');
-    return this.computedSource.filter(item => regexp.test(item[this.labelKey]));
+    const inputMatched = this.filterInput
+      ? this.computedSource.filter(item => regexp.test(item[this.labelKey]))
+      : [];
+    const valueMatched = this.value
+      ? this.computedSource.filter(item => (this.multiple
+        ? this.value.includes(item[this.valueKey])
+        : String(this.value) === String(item[this.valueKey])))
+      : [];
+
+    return [...inputMatched, ...valueMatched].filter(
+      (item, index, arr) => !arr.find((_, i) => _.id === item.id && i > index),
+    );
   }
 
   protected get limitedSource() {
     if (this.limit) {
-      return this.filteredSource && this.filteredSource.slice(0, this.limit);
+      return (
+        this.filteredSource
+          && this.filteredSource.slice(0, this.multiple
+            ? (this.limit + this.value.length)
+            : this.limit)
+      );
     }
+
     return this.filteredSource;
   }
 
   public onFilterSource(val) {
     this.filterInput = val;
   }
-
 
   onInputFn(val) {
     this.$emit('input', val);
